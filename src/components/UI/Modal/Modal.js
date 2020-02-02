af24eac39c696840a95866a73647237430baa164
Abhishek Rai <raigon48@gmail.com>
2020-02-02 17:07:22 +0530

    improve performance by adding should component update in Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,25 @@
-import React from 'react' ;
+import React, { Component } from 'react' ;
 import Cover from '../../../hoc/Cover/Cover';
 import Backdrop from '../Backdrop/Backdrop';
 import CSS from './Modal.module.css';
 
-const modal = (props) => {
-    return (
-        <Cover>
-            <div className={CSS.Modal} style={{transform : props.display ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity : props.display ? '1' : '0'}}>
-                {props.children}
-            </div>
-            <Backdrop show={props.display} cancel={props.cancelOrder}/>
-        </Cover>
-    );
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.display !== this.props.display;
+    }
+
+    render() {
+        return (
+            <Cover>
+                <div className={CSS.Modal} style={{transform : props.display ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity : props.display ? '1' : '0'}}>
+                    {props.children}
+                </div>
+                <Backdrop show={props.display} cancel={props.cancelOrder}/>
+            </Cover>
+        );
+    }
 }
 
-export default modal;
+export default Modal;
